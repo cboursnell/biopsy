b0138425169b52bc0461896cf5c34c93370b47c5
Chris Boursnell <cboursnell@gmail.com>
2014-05-22 14:31:15 +0100

    run 3 threads and stop when they converge on the global optimum

--- test/test_tabu.rb
+++ test/test_tabu.rb
@@ -465,6 +465,36 @@ class TestTabu < Test::Unit::TestCase
     end
 
     should "get multiple threads to converge" do
+      ranges = { :a => (-10..20).to_a, 
+                 :b => (-10..20).to_a, 
+                 :c => (-10..20).to_a }
+
+      search = Biopsy::TabuSearch.new(ranges, 3)
+      start = []
+      start << { :a => 23, :b => 23, :c => 23 }
+      start << { :a => 5, :b => 3, :c => 2 }
+      start << { :a => 20, :b => 5, :c => 20 }
+      search.setup(start)
+
+      current = search.threads[0].current[:parameters]
+      4000.times do |i|
+        result = Helper.sinusoidal(ranges, current)
+        # puts "<Test> got score #{result} from #{current}"
+        current = search.run_one_iteration(current, result)
+      end
+      assert_equal search.threads[0].best[:score], 2.0
+      assert_equal search.threads[0].best[:parameters][:a], 10
+      assert_equal search.threads[0].best[:parameters][:b], 10
+      assert_equal search.threads[0].best[:parameters][:c], 10
+      assert_equal search.threads[1].best[:score], 2.0
+      assert_equal search.threads[1].best[:parameters][:a], 10
+      assert_equal search.threads[1].best[:parameters][:b], 10
+      assert_equal search.threads[1].best[:parameters][:c], 10
+      assert_equal search.threads[2].best[:score], 2.0
+      assert_equal search.threads[2].best[:parameters][:a], 10
+      assert_equal search.threads[2].best[:parameters][:b], 10
+      assert_equal search.threads[2].best[:parameters][:c], 10
+    end
 
     end
 
